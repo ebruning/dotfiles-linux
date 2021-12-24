a975b88dbb1272f75c3c545a537c330112a9c4ba
Ethan Bruning <ethan@bruning.io>
2021-12-24 07:34:49 -0800

    Refactor polybar config Rename bar to bspwm-bar moved modules into modules.ini enabled font awesome

--- config/polybar/bspwm.sh
+++ config/polybar/bspwm.sh
@@ -6,8 +6,8 @@
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload bspwm-bar &
   done
 else
- polybar --reload example &
+ polybar --reload bspwm &
 fi
